8c56f55b70b2b976c6977538d0a9e19dcfc6fa57
Lucas Farias <lucasf1@gmail.com>
2023-04-30 10:55:47 -0300

    add Readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descrição curso Java");
+        curso.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição de Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
